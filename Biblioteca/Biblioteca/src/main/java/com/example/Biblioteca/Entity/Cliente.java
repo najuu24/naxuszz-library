8c86bc8bf48111af13462e12ba295b4071ee004b
najuu24 <anajulia.vieira.tds24@gmail.com>
2025-04-10 15:20:04 -0300

    carlos

---
+++ Biblioteca/Biblioteca/src/main/java/com/example/Biblioteca/Entity/Cliente.java
@@ -0,0 +1,20 @@
+package com.example.Biblioteca.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cliente implements{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String sobrenome;
+    @Column(unique = true)
+    private int cpf;
+}
